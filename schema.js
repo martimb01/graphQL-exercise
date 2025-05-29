605251834324fd0baade121264cf12881866deb6
martimb01 <85357387+martimb01@users.noreply.github.com>
2025-05-29 21:05:21 +0100

    Initial schema and mock DB setup

--- schema.js
+++ schema.js
@@ -1,5 +1,24 @@
 export const typeDefs = `#graphql
- type 
+ type Game {
+    id: ID!,
+    title: String!,
+    plataform: [String!]!
+ }
+ type Review {
+    id: ID!,
+    rating: Int!,
+    content: String!
+    }
+ type Author {
+ id: ID!
+ name: String!
+ verified: Boolean!
+ }
+ type Query {
+    reviews: [Reviews],
+    games: [Game],
+    authors: [Author]
+ }
 `
 
 
